2dac8170540285ecbd4ed2d9222ec641ab43f336
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2025-01-09 22:33:09 -0300

    init: problema 1

---
+++ GupyQuestions/P1/Problema.cs
@@ -0,0 +1,30 @@
+﻿namespace TargetSistemas.P1;
+
+using static Utils;
+
+public class Problema
+{
+    static void main(string[] args)
+    {
+        ConsoleTitle("Resolução para o problema 1");
+        Console.WriteLine(P1());
+        Console.WriteLine("Hello, World!");
+    }
+    
+    // 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+    // Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+    // Imprimir(SOMA);
+    // Ao final do processamento, qual será o valor da variável SOMA?
+    static int P1()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+        
+        while (K < INDICE)
+        {
+            K++;
+            SOMA += K;
+        }
+
+        return SOMA;
+    }
+}
---
+++ GupyQuestions/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace TargetSistemas;
+
+public static class Utils
+{
+    public static void ConsoleTitle(String text)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+}
